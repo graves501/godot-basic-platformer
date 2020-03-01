62f76c4bc214872d930b152a152e763892be6217
graves501 <graves501@protonmail.com>
2020-03-01 19:28:44 +0100

    Player: Add animations, add camera, fix movement handling

--- objects/agents/Player.gd
+++ objects/agents/Player.gd
@@ -30,8 +30,29 @@ func handle_movement(delta):
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	pass
+	
+func handle_animation():
+	var is_moving_horizontally = false
+	
+	if Input.is_action_pressed("move_left"):
+		is_moving_horizontally = true
+		$AnimatedSprite.flip_h = true
+	elif Input.is_action_pressed("move_right"):
+		is_moving_horizontally = true
+		$AnimatedSprite.flip_h = false
+		
+	if is_on_floor():
+		if is_moving_horizontally:
+			$AnimatedSprite.play("Walk")
+		else: 
+			$AnimatedSprite.play("Idle")
+		
+	else:
+		$AnimatedSprite.play("Jump")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
 	handle_movement(delta)
+	handle_animation()
 	pass
